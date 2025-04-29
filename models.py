0f7f5d3248227a3c1e6ccea6d53170dee7c53fa3
Christopher Young <c.t.ythegamer@gmail.com>
2025-04-29 14:40:05 -0600

    Experimentation

--- models.py
+++ models.py
@@ -6,3 +6,12 @@ def avg_per_color(df):
     avg_per_color = df.groupby("color_identity")["pick_number"].mean()
     avg_per_color.plot(kind='bar', title='Avg Pick # by Color')
     plt.show()
+
+def win_rate_vs_ata(card_data):
+    sns.scatterplot(x="GD WR", y="ATA", data=card_data, hue="color_identity")
+    plt.show()
+
+def win_rate_per_color(card_data):
+    avg_per_color = card_data.groupby("color_identity")["GD WR"].mean()
+    avg_per_color.plot(kind="bar")
+    plt.show()
