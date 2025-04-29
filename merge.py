10c00d6fa1a3979906c7b669ded65088a652e589
Nathan Howell <pokreman06@gmail.com>
2025-04-29 13:51:05 -0600

    added compiling

--- merge.py
+++ merge.py
@@ -1,7 +1,7 @@
 import pandas as pd
 # add columns color, rarity, mana cost, etc.
-def add_card_data(df, card_data):
-    combined_df = df.merge(card_data[['pick', 'mana_cost', 'color_identity', 'rarity']], 
-                            on='pick', 
+def add_card_data(df, card_data, columns_to_add):
+    combined_df = df.merge(card_data[columns_to_add], 
+                            on='name', 
                             how='left')
     return combined_df
