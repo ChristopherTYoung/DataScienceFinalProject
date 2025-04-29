048e77f5ac20858c6ecd47b4d5984e7cb3e84deb
Nathan Howell <pokreman06@gmail.com>
2025-04-29 14:03:46 -0600

    added compiling

--- compile.py
+++ compile.py
@@ -1,7 +1,8 @@
+from typing import List
 import pandas as pd 
 from merge import add_card_data
 
-def compile_cards(df, card_data, columns_to_add, row):
+def compile_cards(df: pd.DataFrame, card_data: pd.DataFrame, columns_to_add: List[str], row: int) -> pd.DataFrame:
     newdf = pd.DataFrame({"name": df["cards_in_pack"][row]})
     newdf = add_card_data(newdf, card_data, columns_to_add)
     newdf["draft_id"], newdf["pack_number"], newdf["pick_number"] = df["draft_id"].iloc[row], df["pack_number"].iloc[row], df["pick_number"].iloc[row]
--- merge.py
+++ merge.py
@@ -1,8 +1,9 @@
+from typing import List
 import pandas as pd
 
 
 # add columns color, rarity, mana cost, etc.
-def add_card_data(df, card_data, columns_to_add):
+def add_card_data(df: pd.DataFrame, card_data: pd.DataFrame, columns_to_add: List[str]) -> pd.DataFrame:
     combined_df = df.merge(card_data[columns_to_add], 
                             on='name', 
                             how='left')
